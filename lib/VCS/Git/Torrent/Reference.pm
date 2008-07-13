f4df3f8b1b7010a9ec6a09d83dc1a2b401f20e1a
Joshua Roys <roysjosh@gmail.com>
2008-07-13 19:24:29 -0400

    Reading of Reference objects from git tags

--- lib/VCS/Git/Torrent/Reference.pm
+++ lib/VCS/Git/Torrent/Reference.pm
@@ -14,48 +14,162 @@ has 'torrent' =>
 has 'tag_id' =>
 	isa => "VCS::Git::Torrent::git_object_id",
 	is => "ro",
-	#required => 1
-	;
+	required => 1;
 
 has 'tagged_object' =>
 	isa => "VCS::Git::Torrent::git_object_id",
 	is => "ro",
-	#required => 1
-	;
+	required => 1,
+	lazy => 1,
+	default => \&buildTaggedObject;
 
 has 'tagger' =>
 	isa => "Str",
 	is => "ro",
-	#required => 1
-	;
+	required => 1,
+	lazy => 1,
+	default => \&buildTagger;
 
 has 'tagdate' =>
-	isa => "TimestampTZ",
-	#required => 1,
-	coerce => 1,
+	isa => "Str",
+#	isa => "TimestampTZ",
 	is => "ro",
+	required => 1,
 	lazy => 1,
-	default => \&timestamptz;
+#	coerce => 1,
+	default => \&buildTagDate;
 
 has 'comment' =>
 	isa => "Str",
-	#required => 1,
-	is => "ro";
+	is => "ro",
+	required => 1,
+	lazy => 1,
+	default => \&buildComment;
 
 has 'refs' =>
-	is => "ro",
 	isa => "HashRef[VCS::Git::Torrent::sha1_hex]",
-	#required => 1
-	;
+	is => "ro",
+	required => 1,
+	lazy => 1,
+	default => \&buildRefs;
 
-sub BUILD {
+sub buildComment {
+	my $self = shift;
+	my @data;
+	my $comment;
+
+	if ( $self->tag_id ) {
+		unless ( $self->{_data} ) {
+			@data = $self->git->command('cat-file', 'tag', $self->tag_id);
+			$self->{_data} = \@data;
+		}
+
+		foreach(@{$self->{_data}}) {
+			if ( /^tag (.*)$/ ) {
+				$comment = $1;
+				last;
+			}
+		}
+	}
+
+	$comment;
+}
+
+sub buildRefs {
+	my $self = shift;
+	my @data;
+	my %refs;
+
+	if ( $self->tag_id ) {
+		unless ( $self->{_data} ) {
+			@data = $self->git->command('cat-file', 'tag', $self->tag_id);
+			$self->{_data} = \@data;
+		}
+
+		foreach(@{$self->{_data}}) {
+			if ( /^([0-9a-f]{40})\s+(.*)$/ ) {
+				$refs{$2} = $1;
+			}
+		}
+	}
+
+	\%refs;
+}
+
+sub buildTagDate {
+	my $self = shift;
+	my @data;
+	my $time;
+
+	if ( $self->tag_id ) {
+		unless ( $self->{_data} ) {
+			@data = $self->git->command('cat-file', 'tag', $self->tag_id);
+			$self->{_data} = \@data;
+		}
+
+		foreach(@{$self->{_data}}) {
+			if ( /^tagger\s+(.*)$/ ) {
+				(undef, undef, $time) = $self->git->ident($1);
+				last;
+			}
+		}
+	}
+
+	$time;
+}
+
+sub buildTaggedObject {
+	my $self = shift;
+	my @data;
+	my $tobj;
+
+	if ( $self->tag_id ) {
+		unless ( $self->{_data} ) {
+			@data = $self->git->command('cat-file', 'tag', $self->tag_id);
+			$self->{_data} = \@data;
+		}
+
+		foreach(@{$self->{_data}}) {
+			if ( /^object ([0-9a-f]{40})$/ ) {
+				$tobj = $1;
+				last;
+			}
+		}
+	}
+
+	$tobj;
+}
+
+sub buildTagger {
+	my $self = shift;
+	my @data;
+	my ($name, $email);
+	my $tagger;
+
+	if ( $self->tag_id ) {
+		unless ( $self->{_data} ) {
+			@data = $self->git->command('cat-file', 'tag', $self->tag_id);
+			$self->{_data} = \@data;
+		}
+
+		foreach(@{$self->{_data}}) {
+			if ( /^tagger\s+(.*)$/ ) {
+				($name, $email, undef) = $self->git->ident($1);
+				$tagger = "$name <$email>";
+				last;
+			}
+		}
+	}
+
+	$tagger;
+}
+
+sub buildTag {
 	my $self = shift;
 	my $tag_id = $self->tag_id;
-	if ( $tag_id ) {
-		# loading a reference from disk
-		my @data = $self->git->command("cat-file", "tag", $tag_id);
+	my $line;
 
-		# ... parse into the relevant fields ...
+	if ( $tag_id ) {
 	}
 	else {
 		# creating a references object from scratch
--- t/41-peer-references.t
+++ t/41-peer-references.t
@@ -6,5 +6,35 @@ use warnings;
 
 # This test script tests the VCS::Git::Torrent::References class
 
+BEGIN { use_ok('t::TestUtils') }
+BEGIN { use_ok('VCS::Git::Torrent') }
+BEGIN { use_ok('VCS::Git::Torrent::Reference') }
 
+my $path = mk_tmp_repo();
+chdir($path);
+my $repo = Git->repository();
+my $tag_id = $repo->command('hash-object', '-w', '-t', 'tag', '../41-peer-references/01-simple.tag');
+chomp $tag_id;
+
+my $t = VCS::Git::Torrent->new(
+	repo_hash => '1415' x 10,
+	git => $repo,
+);
+
+my $ref = VCS::Git::Torrent::Reference->new(
+	torrent => $t,
+	tag_id => $tag_id,
+);
+
+is($ref->tag_id, $tag_id, 'tag_id matches');
+is($ref->tagged_object, '5e8f6a7807a378259daa3b91314c8c9775fa160e', 'tagged_object matches');
+is($ref->tagger, 'Sam Vilain <sam@vilain.net>', 'tagger matches');
+is($ref->tagdate, '1215919513 +1200', 'tagdate matches');
+is($ref->comment, '01-simple', 'comment matches');
+
+#foreach(keys(%{$ref->refs})) {
+#	print $_ . "\t" . $ref->{refs}->{$_} . "\n";
+#}
+
+is($ref->refs->{'refs/heads/master'}, '5e8f6a7807a378259daa3b91314c8c9775fa160e', 'refs matches');
 
